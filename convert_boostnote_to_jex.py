9517dca9d19c7c0253f4ee7d6aab41bd060fcb4b
Elliot <elliot@bitoffdev.com>
2021-08-16 18:53:30 -0400

    Convert Boostnote internal links to Joplin
    - Convert Boostnote Note IDs to Joplin. Boostnote has generated a couple
      different types of IDs over the years, including 10-byte keys
      represented via hex as well as standard hyphenated UUID4s. Joplin only
      supports unhyphenated 16-byte keys represented as hex, so this patch
      employs a few different strategies to perform this mapping.
    - Convert the internal link format from Boostnote to the format expected
      by Joplin

--- convert_boostnote_to_jex.py
+++ convert_boostnote_to_jex.py
@@ -1,3 +1,4 @@
+import re
 import tempfile
 import uuid
 
@@ -5,8 +6,53 @@ import boostnote
 import joplin
 
 
-col = boostnote.BoostnoteCollection.from_dir('/data/offline/notes')
-output_location = tempfile.mktemp(suffix='.jex')
+def joplin_uuid() -> str:
+    return str(uuid.uuid4()).replace("-", "")
+
+
+# UUID4_REGEX = "^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$"
+
+
+def boostnote_to_joplin_id(boostnote_id: str):
+    # If the boostnote ID is already in the correct form, return it.
+    if len(boostnote_id) == 32 and re.match(r"^[0-9a-fA-F]*$", boostnote_id):
+        return boostnote_id
+    # At one point, Boostnote was generating 10-byte keys represented via hex.
+    #
+    # As a workaround, we'll prepend 6 hard-coded bytes. Yes, this is a little
+    # jank. However, a conflict is exorbitantly unlikely to occur.
+    if len(boostnote_id) == 20 and re.match(r"^[0-9a-fA-F]*$", boostnote_id):
+        return f"b0057b005700{boostnote_id}"
+    # If the boostnote ID is a standard UUID, we can just strip out the hyphens.
+    uuid4_prog = re.compile(
+        r"^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$"
+    )
+    if uuid4_prog.match(boostnote_id):
+        return boostnote_id.replace("-", "")
+    # Otherwise, the conversion is poorly defined. Generate a new ID.
+    print(f"No direct conversion from boostnote ID {boostnote_id} to Joplin")
+    return joplin_uuid()
+
+
+def replace_boostnote_links_with_joplin_links(
+    content: str, map_boostnote_to_joplin: dict
+) -> str:
+    def get_replacement(m: re.Match) -> str:
+        boostnote_id = m.group(2)
+        joplin_id = map_boostnote_to_joplin.get(boostnote_id)
+        if joplin_id is None:
+            print(f"Could not find replacement link for {joplin_id}")
+            joplin_id = boostnote_id
+        repl = f"[{m.group(1)}](:/{joplin_id})"
+        print(f"Replaced link: {repl}")
+        return repl
+
+    prog = re.compile(r"\[([^\]]*)\]\(:note:([^\)]*)\)")
+    return prog.sub(get_replacement, content)
+
+
+col = boostnote.BoostnoteCollection.from_dir("/data/offline/notes")
+output_location = tempfile.mktemp(suffix=".jex")
 store = joplin.JoplinTarStore(output_location)
 for folder_id in col.meta.list_folder_ids():
     folder_name = col.meta.get_folder_name(folder_id)
@@ -14,7 +60,6 @@ for folder_id in col.meta.list_folder_ids():
     joplin_entity = joplin.joplin_create_folder(folder_id, folder_name)
     payload = joplin.unparse_joplin_note(joplin_entity)
     store.write(f"{folder_id}.md", payload)
-
 
 
 # create tags
@@ -28,13 +73,29 @@ for tag_name in col.list_tags():
     store.write(f"{tag_id}.md", payload)
 
 
+boostnote_entities = list(col.get_entities())
+map_boostnote_to_joplin = {}
+for e in boostnote_entities:
+    new_id = boostnote_to_joplin_id(e.id)
+    map_boostnote_to_joplin[e.id] = new_id
+    e.id = new_id
 
 # create notes
-for boostnote_entity in col.get_entities():
+for boostnote_entity in boostnote_entities:
     if not isinstance(boostnote_entity, boostnote.BoostnoteNote):
         continue
+
     print(f"Creating Joplin note for Boostnote note with ID {boostnote_entity.id}")
-    joplin_entity = joplin.joplin_create_note(boostnote_entity.id, boostnote_entity.title, boostnote_entity.content, boostnote_entity.folder_id, boostnote_entity.created_at, boostnote_entity.updated_at)
+    joplin_entity = joplin.joplin_create_note(
+        boostnote_entity.id,
+        boostnote_entity.title,
+        replace_boostnote_links_with_joplin_links(
+            boostnote_entity.content, map_boostnote_to_joplin
+        ),
+        boostnote_entity.folder_id,
+        boostnote_entity.created_at,
+        boostnote_entity.updated_at,
+    )
     # print(joplin_entity.headers)
     payload = joplin.unparse_joplin_note(joplin_entity)
     store.write(f"{boostnote_entity.id}.md", payload)
@@ -42,10 +103,13 @@ for boostnote_entity in col.get_entities():
     for tag_name in boostnote_entity.tags:
         notetag_id = str(uuid.uuid4())
         tag_id = tag_name_to_id[tag_name]
-        joplin_notetag_entity = joplin.joplin_create_notetag(notetag_id, boostnote_entity.id, tag_id)
-        store.write(f"{notetag_id}.md", joplin.unparse_joplin_note(joplin_notetag_entity))
+        joplin_notetag_entity = joplin.joplin_create_notetag(
+            notetag_id, boostnote_entity.id, tag_id
+        )
+        store.write(
+            f"{notetag_id}.md", joplin.unparse_joplin_note(joplin_notetag_entity)
+        )
         print(f"Wrote NoteTag with ID {notetag_id}")
 
 
 print(f"Saved output to {output_location}")
-

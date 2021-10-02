cc74ded44a04b72e04423849d8cfab9feb6a86ba
Elliot <elliot@bitoffdev.com>
2021-10-02 13:03:59 -0400

    Single entrypoint: main.py

--- convert_boostnote_to_jex.py
+++ convert_boostnote_to_jex.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import mimetypes
 import tempfile
 import uuid
@@ -73,84 +74,89 @@ def replace_boostnote_links_with_joplin_links(
     return content
 
 
-col = boostnote.BoostnoteCollection.from_dir("/data/offline/notes")
-output_location = tempfile.mktemp(suffix=".jex")
-store = joplin.JoplinTarStore(output_location)
-for folder_id in col.meta.list_folder_ids():
-    folder_name = col.meta.get_folder_name(folder_id)
-    print(f"Writing folder {folder_name} to Joplin store")
-    joplin_entity = joplin.joplin_create_folder(folder_id, folder_name)
-    payload = joplin.unparse_joplin_note(joplin_entity)
-    store.write(f"{folder_id}.md", payload)
-
-
-# create tags
-tag_name_to_id = {}
-for tag_name in col.list_tags():
-    print(f"Writing tag to Joplin store: {tag_name}")
-    tag_id = str(uuid.uuid4())
-    tag_name_to_id[tag_name] = tag_id
-    joplin_tag = joplin.joplin_create_tag(tag_id, tag_name)
-    payload = joplin.unparse_joplin_note(joplin_tag)
-    store.write(f"{tag_id}.md", payload)
-
-
-# create resources
-boostnote_attachments = list(col.get_attachments())
-map_boostnote_attachment_to_joplin_id = {}
-for attachment in col.get_attachments():
-    attachment_id = joplin_uuid()
-    map_boostnote_attachment_to_joplin_id[attachment] = attachment_id
-    print(f"Writing attachment meta to Joplin store: {attachment}")
-    joplin_resource = joplin.joplin_create_resource(attachment_id, attachment.filename)
-    payload = joplin.unparse_joplin_note(joplin_resource)
-    store.write(f"{attachment_id}.md", payload)
-
-    print(f"Writing attachment blob to Joplin store: {attachment}")
-    ext = attachment.filename.rsplit(".", 1)[-1]
-    store.write_bin(f"resources/{attachment_id}.{ext}", col.read_attachment(attachment))
-
-
-# create Joplin-accepted IDs for all boostnote notes
-boostnote_entities = list(col.get_entities())
-map_boostnote_to_joplin = {}
-for e in boostnote_entities:
-    new_id = boostnote_to_joplin_id(e.id)
-    map_boostnote_to_joplin[e.id] = new_id
-    e.id = new_id
-
-# create notes
-for boostnote_entity in boostnote_entities:
-    if not isinstance(boostnote_entity, boostnote.BoostnoteNote):
-        continue
-
-    print(f"Creating Joplin note for Boostnote note with ID {boostnote_entity.id}")
-    joplin_entity = joplin.joplin_create_note(
-        boostnote_entity.id,
-        boostnote_entity.title,
-        replace_boostnote_links_with_joplin_links(
-            boostnote_entity.content,
-            map_boostnote_to_joplin,
-            map_boostnote_attachment_to_joplin_id,
-        ),
-        boostnote_entity.folder_id,
-        boostnote_entity.created_at,
-        boostnote_entity.updated_at,
-    )
-    # print(joplin_entity.headers)
-    payload = joplin.unparse_joplin_note(joplin_entity)
-    store.write(f"{boostnote_entity.id}.md", payload)
-    # Create entities tagging notes
-    for tag_name in boostnote_entity.tags:
-        notetag_id = str(uuid.uuid4())
-        tag_id = tag_name_to_id[tag_name]
-        joplin_notetag_entity = joplin.joplin_create_notetag(
-            notetag_id, boostnote_entity.id, tag_id
+def main(boost_dir_path: str):
+    col = boostnote.BoostnoteCollection.from_dir(boost_dir_path)
+    output_location = tempfile.mktemp(suffix=".jex")
+    store = joplin.JoplinTarStore(output_location)
+    for folder_id in col.meta.list_folder_ids():
+        folder_name = col.meta.get_folder_name(folder_id)
+        print(f"Writing folder {folder_name} to Joplin store")
+        joplin_entity = joplin.joplin_create_folder(folder_id, folder_name)
+        payload = joplin.unparse_joplin_note(joplin_entity)
+        store.write(f"{folder_id}.md", payload)
+    
+    
+    # create tags
+    tag_name_to_id = {}
+    for tag_name in col.list_tags():
+        print(f"Writing tag to Joplin store: {tag_name}")
+        tag_id = str(uuid.uuid4())
+        tag_name_to_id[tag_name] = tag_id
+        joplin_tag = joplin.joplin_create_tag(tag_id, tag_name)
+        payload = joplin.unparse_joplin_note(joplin_tag)
+        store.write(f"{tag_id}.md", payload)
+    
+    
+    # create resources
+    boostnote_attachments = list(col.get_attachments())
+    map_boostnote_attachment_to_joplin_id = {}
+    for attachment in col.get_attachments():
+        attachment_id = joplin_uuid()
+        map_boostnote_attachment_to_joplin_id[attachment] = attachment_id
+        print(f"Writing attachment meta to Joplin store: {attachment}")
+        joplin_resource = joplin.joplin_create_resource(attachment_id, attachment.filename)
+        payload = joplin.unparse_joplin_note(joplin_resource)
+        store.write(f"{attachment_id}.md", payload)
+    
+        print(f"Writing attachment blob to Joplin store: {attachment}")
+        ext = attachment.filename.rsplit(".", 1)[-1]
+        store.write_bin(f"resources/{attachment_id}.{ext}", col.read_attachment(attachment))
+    
+    
+    # create Joplin-accepted IDs for all boostnote notes
+    boostnote_entities = list(col.get_entities())
+    map_boostnote_to_joplin = {}
+    for e in boostnote_entities:
+        new_id = boostnote_to_joplin_id(e.id)
+        map_boostnote_to_joplin[e.id] = new_id
+        e.id = new_id
+    
+    # create notes
+    for boostnote_entity in boostnote_entities:
+        if not isinstance(boostnote_entity, boostnote.BoostnoteNote):
+            continue
+    
+        print(f"Creating Joplin note for Boostnote note with ID {boostnote_entity.id}")
+        joplin_entity = joplin.joplin_create_note(
+            boostnote_entity.id,
+            boostnote_entity.title,
+            replace_boostnote_links_with_joplin_links(
+                boostnote_entity.content,
+                map_boostnote_to_joplin,
+                map_boostnote_attachment_to_joplin_id,
+            ),
+            boostnote_entity.folder_id,
+            boostnote_entity.created_at,
+            boostnote_entity.updated_at,
         )
-        store.write(
-            f"{notetag_id}.md", joplin.unparse_joplin_note(joplin_notetag_entity)
-        )
-        print(f"Wrote NoteTag with ID {notetag_id}")
+        # print(joplin_entity.headers)
+        payload = joplin.unparse_joplin_note(joplin_entity)
+        store.write(f"{boostnote_entity.id}.md", payload)
+        # Create entities tagging notes
+        for tag_name in boostnote_entity.tags:
+            notetag_id = str(uuid.uuid4())
+            tag_id = tag_name_to_id[tag_name]
+            joplin_notetag_entity = joplin.joplin_create_notetag(
+                notetag_id, boostnote_entity.id, tag_id
+            )
+            store.write(
+                f"{notetag_id}.md", joplin.unparse_joplin_note(joplin_notetag_entity)
+            )
+            print(f"Wrote NoteTag with ID {notetag_id}")
+    
+    
+    print(f"Saved output to {output_location}")
 
 
-print(f"Saved output to {output_location}")
+if __name__ == "__main__":
+    main(sys.argv[1])
--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def argparse_install_boostnote_stats(parser: argparse.ArgumentParser):
     parser.add_argument("path")
 
 
+def argparse_install_boost2jex(parser: argparse.ArgumentParser):
+    parser.add_argument("path")
+
+
+def argparse_install_jex2boost(parser: argparse.ArgumentParser):
+    parser.add_argument("path")
+
+
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="cmd")
@@ -19,13 +27,26 @@ def main():
     boostnote_stats_parser = subparsers.add_parser("boostnote-stats")
     argparse_install_boostnote_stats(boostnote_stats_parser)
 
+    boost2jex_parser = subparsers.add_parser("boost2jex")
+    argparse_install_boost2jex(boost2jex_parser)
+
+    jex2boost_parser = subparsers.add_parser("jex2boost")
+    argparse_install_jex2boost(jex2boost_parser)
+
     args = parser.parse_args()
 
     if args.cmd == "boostnote-stats":
         boostnote_stats(args)
+    elif args.cmd == "boost2jex":
+        import convert_boostnote_to_jex as boost2jex
+        boost2jex.main(args.path)
+    elif args.cmd == "jex2boost":
+        import convert_jex_to_boostnote as jex2boost
+        jex2boost.main(args.path)
     else:
         parser.print_help()
 
 
 if __name__ == "__main__":
     main()
+
